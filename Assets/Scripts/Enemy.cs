3d5a2d6446a999f031b33ba1f9e9173759e1bdf9
balabin Anton <aa7224@gmail.com>
2020-12-21 13:26:07 +0200

    Intial

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private EnemySO scriptableObject;
+
+    private float _Health;
+    private float _Speed;
+    private Transform _Target;
+
+    private int _WaypointIndex = 0;
+    
+    void Start()
+    {
+        _Health = scriptableObject.health;
+        _Speed = scriptableObject.speed;
+
+        _Target = Waypoints._Points[0];
+    }
+
+    
+    void Update()
+    {
+        Vector3 dir = _Target.position - transform.position;
+        transform.Translate(dir.normalized * _Speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, _Target.position) <= 0.15f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (_WaypointIndex >= Waypoints._Points.Length - 1)
+            return;
+        
+        _WaypointIndex++;
+        _Target = Waypoints._Points[_WaypointIndex];
+    }
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+   private static Transform[] _points;
+   public static Transform[] _Points => _points;
+
+   private void Awake()
+   {
+      _points = new Transform[transform.childCount];
+      for (int i = 0; i < _points.Length; i++)
+      {
+         _points[i] = transform.GetChild(i);
+      }
+   }
+}
